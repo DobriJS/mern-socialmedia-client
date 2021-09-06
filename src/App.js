e4bb11de9d1d6e4448421ce4cfc0ec71a3c6170a
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-07 00:28:16 +0300

    Dispatch getPosts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
 
 const App = () =>  {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxWidth="lg">
