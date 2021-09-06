72c0b9d16019cdca9b527eacea38cd8f717c0c81
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-06 23:27:42 +0300

    Add styles for main component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
 
 const App = () =>  {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Feed</Typography>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Feed</Typography>
       </AppBar>
       <Grow in>
         <Container>
