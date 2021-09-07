4ea884ab0cb560fa2840e208c3973b1294e8bc0b
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-07 17:28:58 +0300

    Install moment js

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Post from './Post/Post';
 import { useSelector } from 'react-redux';
-import { Grid } from '@material-ui/core';
+import { Grid, CircularProgress } from '@material-ui/core';
 import useStyles from './styles';
 
 const Posts = () => {
@@ -9,9 +9,15 @@ const Posts = () => {
   const classes = useStyles();
 
   return (
-    <Grid className={classes.mainContainer}>
-      <Post />
-    </Grid>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 };
 
