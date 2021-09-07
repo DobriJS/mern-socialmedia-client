be2491e3d6e9991998d378eafa416d24d688c035
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-07 12:06:54 +0300

    Add new styles for Post

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Card } from '@material-ui/core/';
 import useStyles from './styles';
 
 const Post = () => {
   const classes = useStyles();
 
   return (
-    <h1>POSTS</h1>
+    <Card className={classes.card}>
+      <h1>POST</h1>
+    </Card>
   );
 };
 
