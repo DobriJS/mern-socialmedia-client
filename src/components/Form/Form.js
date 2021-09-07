0c842a6ea69b82c488132feb58e003abe402dcdc
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-07 12:21:59 +0300

    Apply Textfield to Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Typography, Paper } from '@material-ui/core';
 import useStyles from './styles';
 
 const Form = () => {
   const classes = useStyles();
+  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <h1>
-        Form
-    </h1>
+    <Paper className={classes.paper}>
+      <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+        <Typography variant='h6'>Create a Feed</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator} />
+      </form>
+    </Paper>
   );
 };
 
