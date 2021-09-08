ece2f69fbbdc0356074ebac671f6ffd4b5e4f166
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-09 01:57:12 +0300

    Dispatch delete post

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -4,9 +4,13 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
 import useStyles from './styles';
 
+import { deletePost } from '../../../actions/posts';
+
 const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
   const classes = useStyles();
 
   return (
@@ -60,9 +64,11 @@ const Post = ({ post, setCurrentId }) => {
           <ThumbUpAltIcon fontSize="small" />
            Like {post.likeCount}
         </Button>
-        <Button size="small" color="primary">
-          <DeleteIcon fontSize="small" />
-           Delete
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" />
+          Delete
         </Button>
       </CardActions>
     </Card>
