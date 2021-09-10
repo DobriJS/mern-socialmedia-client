f39fc5046ead6f91778a1c33c7403823b4771dda
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-10 22:38:48 +0300

    Mock user auth and logout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Container, Grow, Grid } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -15,21 +16,23 @@ const App = () =>  {
   }, [currentId, dispatch]);
 
   return (
-    <Container maxWidth="lg">
-      <Navbar />
-      <Grow in>
-        <Container>
-          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Grow in>
+          <Container>
+            <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts setCurrentId={setCurrentId} />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+          </Container>
+        </Grow>
+      </Container>
+    </BrowserRouter>
   );
 };
 
