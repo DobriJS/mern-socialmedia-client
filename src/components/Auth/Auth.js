da4a924322fdc2ab0d51b372a23f05065ff71494
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-11 01:39:22 +0300

    Add Input field

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Input from './Input';
 import useStyles from './styles';
 
 const Auth = () => {
