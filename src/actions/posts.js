b4242910ee4c22056a40266f3cb414dc8a5a3723
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-14 17:54:39 +0300

    Remove whispaces

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -14,7 +14,6 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
-
     dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error.message);
@@ -24,7 +23,6 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
-
     dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error.message);
@@ -34,7 +32,6 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
-
     dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error.message);
@@ -44,7 +41,6 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
-
     dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.log(error.message);
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -45,7 +45,6 @@ const Auth = () => {
 
     try {
       dispatch({ type: AUTH, data: { result, token } });
-
       history.push('/');
     } catch (error) {
       console.log(error);
