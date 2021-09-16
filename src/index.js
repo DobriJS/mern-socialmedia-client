f005c3f5b32a708a007e599455db4e4928481cd2
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-16 15:50:52 +0300

    Configure store Enhancers

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { reducers } from './reducers';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
