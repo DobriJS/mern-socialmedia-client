99b569e2e85b9fd805b561770aa4849bcbd77e3b
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-17 19:38:20 +0300

    Query hooks and set more responsive UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Container, Grow, Grid, Paper } from '@material-ui/core';
+import { useHistory, useLocation } from 'react-router-dom';
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
+import ChipInput from 'material-ui-chip-input';
 import { getPosts } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import Pagination from '../Pagination/Pagination';
+import useStyles from './styles';
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
 
 const Home = () => {
+  const classes = useStyles();
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
+  const query = useQuery();
+  const history = useHistory();
+  const page = query.get('page') || 1;
+  const searchQuery = query.get('searchQuery');
 
   useEffect(() => {
     dispatch(getPosts());
@@ -16,12 +28,22 @@ const Home = () => {
 
   return (
     <Grow in>
-      <Container>
-        <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-          <Grid item xs={12} sm={7}>
+      <Container maxWidth='xl'>
+        <Grid container justifyContent="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar className={classes.appBarSearch} positions='static' color='inherit'>
+              <TextField
+                name='search'
+                variant='outlined'
+                label='Search Posts'
+                fullWidth
+                value='DEMO'
+                onchange={() => {}}
+              />
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6}>
               <Pagination />
