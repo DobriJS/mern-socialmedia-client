f06d86838ab2a2f19daf12336d52ed8fdc7602b3
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-18 02:45:07 +0300

    Search tags and Posts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
-import { ChipInput } from '@material-ui/core';
+import ChipInput from '@material-ui/core';
 import { getPosts } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
@@ -16,15 +16,35 @@ const useQuery = () => {
 const Home = () => {
   const classes = useStyles();
   const [currentId, setCurrentId] = useState(0);
+  const [search, setSearch] = useState('');
+  const [tags, setTags] = useState([]);
+
   const dispatch = useDispatch();
-  const query = useQuery();
   const history = useHistory();
+  const query = useQuery();
   const page = query.get('page') || 1;
   const searchQuery = query.get('searchQuery');
 
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
+
+  const searchPost = () => {
+    if (search.trim()) {
+    // fetch search post
+    } else {
+      history.push('/');
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if(e.keyCode === 13) {
+      searchPost();
+    }
+  };
+
+  const handleAddChip = (tag) => setTags([...tags, tag]);
+  const handleDeleteChip = (chipToDelete) => setTags(tags.filter((tag) => tag !== chipToDelete));
 
   return (
     <Grow in>
@@ -38,11 +58,21 @@ const Home = () => {
               <TextField
                 name='search'
                 variant='outlined'
+                onKeyPress={handleKeyPress}
                 label='Search Posts'
                 fullWidth
-                value='DEMO'
-                onchange={() => {}}
+                value={search}
+                onchange={(e) => setSearch(e.target.value)}
               />
+              <ChipInput
+                style={{ margin: '10px 0' }}
+                value={tags}
+                onAdd={(chip) => handleAddChip(chip)}
+                onDelete={(chip) => handleDeleteChip(chip)}
+                label="Search Tags"
+                variant="outlined"
+              />
+              <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
             </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6}>
