67fdd5419a3677fd4f66489fc67bfa8a83d179c0
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-21 18:29:19 +0300

    Eslint test

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -49,21 +49,21 @@ const PostDetails = () => {
           </Typography>
           <Typography gutterBottom variant="body1" component="p">
             {post?.message}
-            </Typography>
+          </Typography>
           <Typography variant="h6">
             Created by: {post?.name}
-            </Typography>
+          </Typography>
           <Typography variant="body1">
             {moment(post?.createdAt).fromNow()}
-            </Typography>
+          </Typography>
           <Divider style={{ margin: '20px 0' }} />
           <Typography variant="body1"><strong>
             Divide Test 1</strong>
-            </Typography>
+          </Typography>
           <Divider style={{ margin: '20px 0' }} />
           <Typography variant="body1">
             <strong>Divide Test 1</strong>
-            </Typography>
+          </Typography>
           <Divider style={{ margin: '20px 0' }} />
         </div>
         <div className={classes.imageSection}>
@@ -77,23 +77,23 @@ const PostDetails = () => {
         <div className={classes.section}>
           <Typography gutterBottom variant="h5">
             You might also like:
-            </Typography>
+          </Typography>
           <Divider />
           <div className={classes.recommendedPosts}>
             {recommendedPosts.map(({ title, name, message, likes, selectedFile, _id }) => (
               <div style={{ margin: '20px', cursor: 'pointer' }} onClick={() => openPost(_id)} key={_id}>
                 <Typography gutterBottom variant="h6">
                   {title}
-                  </Typography>
+                </Typography>
                 <Typography gutterBottom variant="subtitle2">
                   {name}
-                  </Typography>
+                </Typography>
                 <Typography gutterBottom variant="subtitle2">
                   {message}
-                  </Typography>
+                </Typography>
                 <Typography gutterBottom variant="subtitle1">
                   Likes: {likes.length}
-                  </Typography>
+                </Typography>
                 <img src={selectedFile} width="200px" />
               </div>
             ))}
