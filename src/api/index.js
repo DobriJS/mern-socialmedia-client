1727e0193c857be30f167ecee5a4266c59f44db1
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-01-08 15:21:20 +0200

    API

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 //'https://mern-social-media-feed.herokuapp.com/'
 
-const API = axios.create({ baseURL: 'http://localhost:4000' });
+const API = axios.create({
+  baseURL: 'https://mern-social-media-feed.herokuapp.com/',
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
