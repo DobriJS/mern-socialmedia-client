08bf96b15fa1a615525d334ac77d442d2a15816b
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-03-14 17:23:30 +0200

    Remove React Strict Mode

--- src/index.js
+++ src/index.js
@@ -14,11 +14,8 @@ const composeEnhancers =
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    ,
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
